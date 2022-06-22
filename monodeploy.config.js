b6116a7214af64d05e7a7a2d667b7b28f5896e85
Jan Šilhan <jsilhan@oriflame.com>
2022-06-22 15:03:33 +0200

    ci: Update configuration.

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -2,6 +2,5 @@ module.exports = {
   preset: 'monodeploy/preset-recommended',
   conventionalChangelogConfig: 'conventional-changelog-rajzik',
   changesetIgnorePatterns: ['**/*.test.js'],
-  registryUrl: 'https://registry.npmjs.org',
   packageGroupManifestField: 'group',
 };
