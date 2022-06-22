09d161770325ccb9b42978ddc4529c65b8c016d7
Jan Šilhan <jsilhan@oriflame.com>
2022-06-22 15:51:50 +0200

    ci: Update yarn configs.

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  registryUrl: 'https://registry.npmjs.org',
   preset: 'monodeploy/preset-recommended',
   conventionalChangelogConfig: 'conventional-changelog-rajzik',
   changesetIgnorePatterns: ['**/*.test.js'],
