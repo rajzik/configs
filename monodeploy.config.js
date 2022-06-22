e64163098edeb6599d9f553a88c611d735d6fb65
Jan Šilhan <jsilhan@oriflame.com>
2022-06-22 12:25:48 +0200

    cd: Update monodeploy url.

--- monodeploy.config.js
+++ monodeploy.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'monodeploy/preset-recommended',
   conventionalChangelogConfig: 'conventional-changelog-rajzik',
   changesetIgnorePatterns: ['**/*.test.js'],
+  registryUrl: 'https://registry.npmjs.org',
   packageGroupManifestField: 'group',
 };
